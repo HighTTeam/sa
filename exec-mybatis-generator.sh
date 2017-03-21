1789ec66d36a2fe22fcf0c63d022bd86b0f7fa2d
Neron.Liu <neron.liu@gmail.com>
2017-03-21 23:26:21 +0800

    更新脚本。

--- exec-mybatis-generator.sh
+++ exec-mybatis-generator.sh
@@ -1,13 +1,6 @@
 #!/bin/bash 
 
-JDBC_DRIVER=com.mysql.jdbc.Driver
-JDBC_USERNAME=root
-JDBC_PASSWORD=rootroot
-JDBC_URL=jdbc:mysql://mysql-server:3306/sa?autoReconnect=true&characterEncoding=utf8
-
-mvn -Dmybatis.generator.jdbcDriver=$JDBC_DRIVER \
-    -Dmybatis.generator.jdbcUserId=$JDBC_USERNAME \
-    -Dmybatis.generator.jdbcPassword=$JDBC_PASSWORD \
-    -Dmybatis.generator.jdbcURL=$JDBC_URL \
-    -Dmybatis.generator.overwrite=true \
-    mybatis-generator:generate
+if [ ! -d generated ]; then
+    mkdir generated
+fi
+mvn -Dmybatis.generator.overwrite=true mybatis-generator:generate
